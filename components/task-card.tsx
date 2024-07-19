2966f8fc2ed2879c3d8342deabf25fd54dec05e5
nicolasmendonca <nicolasmendonca@gmail.com>
2024-07-19 15:15:42 -0300

    Re-implement task updates and queries

--- components/task-card.tsx
+++ components/task-card.tsx
@@ -1,112 +1,104 @@
-import { Button } from "@/components/ui/button"
-import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover"
-import { Calendar } from "@/components/ui/calendar"
-import { CalendarDaysIcon, FlagIcon, TrashIcon } from "lucide-react"
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select"
-import { Card, CardContent } from "@/components/ui/card"
-import { Checkbox } from "@/components/ui/checkbox"
-import { db, type Task } from '../lib/db'
-import { format } from 'date-fns/format'
+'use-client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from '@/components/ui/popover';
+import { Calendar } from '@/components/ui/calendar';
+import { CalendarDaysIcon, FlagIcon, TrashIcon } from 'lucide-react';
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from '@/components/ui/select';
+import { Card, CardContent } from '@/components/ui/card';
+import { Checkbox } from '@/components/ui/checkbox';
+import { db, type Task } from '../lib/db';
+import { format } from 'date-fns/format';
 import { produce } from 'immer';
-import useSWRMutation from "swr/mutation";
-import useSWR, {mutate} from 'swr'
 
-
-const updateTask = async (key: string, { arg }: { arg: { task: Task } }) => {
-    await db.tasks.update(arg.task.id, arg.task);
-    return arg
-};
-
-const deleteTask = async (key: string, { arg }: { arg: { task: Task } }) => {
-    await db.tasks.delete(arg.task.id);
-    return undefined
-};
-
-
-export default function TaskCard(props: Readonly<{
-  taskId: Task['id'],
-  onTaskUpdated?: (updatedTask: Task) => void;
-  onTaskDeleted?: () => void;
+export default function TaskCard({
+  task,
+  onTaskUpdated,
+  onTaskDeleted,
+}: Readonly<{
+  task: Task;
+  onTaskUpdated: (updatedTask: Task) => void;
+  onTaskDeleted: () => void;
 }>) {
-  const swrKey = `/tasks/${props.taskId}`
-  const taskQuery = useSWR(swrKey, () => db.tasks.get(props.taskId), {
-    suspense: true
-  })
-  const updateTaskMutation = useSWRMutation(swrKey, updateTask, {
-    onSuccess: ({ task }) => props.onTaskUpdated?.(task)
-  })
-  const deleteTaskMutation = useSWRMutation(swrKey, deleteTask, {
-    onSuccess: () => props.onTaskDeleted?.()
-  })
-
-  const task = taskQuery.data!;
-
-  return <Card>
-    <CardContent className="grid gap-2 pt-6">
-      <div className="flex items-center gap-3">
-        <Checkbox id={`checkbox-task-${task.id}`} checked={task.completed} onCheckedChange={newCheckedState => {
-          const updatedTask = produce(task, draft => {
-            draft.completed = newCheckedState as boolean;
-          })
-          updateTaskMutation.trigger({ task: updatedTask }, {
-            optimisticData: task,
-          })
-        }} />
-        <div className="flex-1">
-          <h3 className="font-medium pl-2">
-            {task.description}
-          </h3>
-          <div className="flex items-center gap-2 mt-2">
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button variant="ghost" className="w-fit px-2 h-3 text-xs font-normal">
-                  <CalendarDaysIcon className='size-4 stroke-muted-foreground mr-1' />
-                  {task.dueDate ? `Due: ${format(task.dueDate, "EEE P")}` : `No due date`}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0" align="start">
-                <Calendar mode="single" selected={task.dueDate ?? undefined} onSelect={(newDay) => {
-                  const updatedTask = produce(task, draft => {
-                    draft.dueDate = newDay ?? null;
-                  })
-                  updateTaskMutation.trigger({ task: updatedTask }, {
-                    optimisticData: task,
-                    onSuccess: () => {
-                      mutate(key => typeof key === 'string' && key.startsWith('/task-groups'), undefined, {
-                        revalidate: true
-                      })
-                    }
-                  })
-                }} />
-              </PopoverContent>
-            </Popover>
-            <Select value={task.projectId ?? undefined} onValueChange={(newProjectValue) => {
-              const updatedTask = produce(task, draft => {
-                draft.projectId = newProjectValue;
-              })
-              updateTaskMutation.trigger({ task: updatedTask }, {
-                optimisticData: task,
-              })
-            }}>
-              <SelectTrigger className="h-3 group w-fit px-2 text-xs border-none hover:bg-muted">
-                <FlagIcon className='stroke-muted-foreground group-hover:stroke-accent-foreground size-4 mr-1' />
-                <SelectValue placeholder="No Project" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="project-a">Project A</SelectItem>
-                <SelectItem value="project-b">Project B</SelectItem>
-                <SelectItem value="project-c">Project C</SelectItem>
-              </SelectContent>
-            </Select>
+  return (
+    <Card>
+      <CardContent className="grid gap-2 pt-6">
+        <div className="flex items-center gap-3">
+          <Checkbox
+            id={`checkbox-task-${task.id}`}
+            checked={task.completed}
+            onCheckedChange={(newCheckedState) => {
+              const updatedTask = produce(task, (draft) => {
+                draft.completed = newCheckedState as boolean;
+              });
+              onTaskUpdated(updatedTask);
+            }}
+          />
+          <div className="flex-1">
+            <h3 className="font-medium pl-2">{task.description}</h3>
+            <div className="flex items-center gap-2 mt-2">
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    className="w-fit px-2 h-3 text-xs font-normal"
+                  >
+                    <CalendarDaysIcon className="size-4 stroke-muted-foreground mr-1" />
+                    {task.dueDate
+                      ? `Due: ${format(task.dueDate, 'EEE P')}`
+                      : `No due date`}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={task.dueDate ?? undefined}
+                    onSelect={(newDay) => {
+                      const updatedTask = produce(task, (draft) => {
+                        draft.dueDate = newDay ?? null;
+                      });
+                      onTaskUpdated(updatedTask);
+                    }}
+                  />
+                </PopoverContent>
+              </Popover>
+              <Select
+                value={task.projectId ?? undefined}
+                onValueChange={(newProjectValue) => {
+                  const updatedTask = produce(task, (draft) => {
+                    draft.projectId = newProjectValue;
+                  });
+                  onTaskUpdated(updatedTask);
+                }}
+              >
+                <SelectTrigger className="h-3 group w-fit px-2 text-xs border-none hover:bg-muted">
+                  <FlagIcon className="stroke-muted-foreground group-hover:stroke-accent-foreground size-4 mr-1" />
+                  <SelectValue placeholder="No Project" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="project-a">Project A</SelectItem>
+                  <SelectItem value="project-b">Project B</SelectItem>
+                  <SelectItem value="project-c">Project C</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
           </div>
+          <Button variant="ghost" size="icon" onClick={onTaskDeleted}>
+            <TrashIcon className="h-5 w-5" />
+            <span className="sr-only">Delete task</span>
+          </Button>
         </div>
-        <Button variant="ghost" size="icon" onClick={() => {
-          deleteTaskMutation.trigger({ task })
-        }}>
-          <TrashIcon className="h-5 w-5" />
-          <span className="sr-only">Delete task</span>
-        </Button>
-      </div>
-    </CardContent>
-  </Card>
+      </CardContent>
+    </Card>
+  );
 }
--- components/tasks-page.tsx
+++ components/tasks-page.tsx
@@ -1,103 +1,188 @@
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import CreateTaskForm from "./create-task-form";
-import { ListIcon, MenuIcon } from "lucide-react";
-import Sidebar from "./sidebar";
-import TaskListGroup from "./task-list-group";
-import { db, Task } from "../lib/db";
-import { addDays, isAfter, isSameDay, startOfDay, startOfWeek, endOfWeek, isWithinInterval, addWeeks } from 'date-fns'
-import { mutate } from 'swr'
+import { Button } from '@/components/ui/button';
+import CreateTaskForm from './create-task-form';
+import { ListIcon, MenuIcon } from 'lucide-react';
+import Sidebar from './sidebar';
+import { db, Task } from '../lib/db';
+import {
+  addDays,
+  isAfter,
+  isSameDay,
+  startOfDay,
+  isWithinInterval,
+} from 'date-fns';
+import useSWR from 'swr';
+import { produce } from 'immer';
+import TaskCard from './task-card';
 
+const getTaskGroups = (tasks: Task[]) =>
+  [
+    {
+      id: 'today',
+      name: 'Today',
+      noTasksLabel: 'No tasks for today',
+      getTasks: () => {
+        function isToday(date: Date) {
+          const today = new Date();
+          return isSameDay(date, today);
+        }
 
-const groups = [
-  {
-    id: "today",
-    name: "Today",
-    noTasksLabel: "No tasks for today",
-    loadTaskIds: async () => {
-      function isToday(date: Date) {
-        const today = new Date();
-        return isSameDay(date, today);
-      }
+        return tasks.filter(
+          (task) => task.dueDate !== null && isToday(task.dueDate)
+        );
+      },
+    },
+    {
+      id: 'tomorrow',
+      name: 'Tomorrow',
+      noTasksLabel: 'No tasks for tomorrow',
+      getTasks: () => {
+        function isTomorrow(date: Date) {
+          const today = new Date();
+          const tomorrow = addDays(today, 1);
+          return isSameDay(date, tomorrow);
+        }
+        return tasks.filter(
+          (task) => task.dueDate !== null && isTomorrow(task.dueDate)
+        );
+      },
+    },
+    {
+      id: 'next-7-days',
+      name: 'Next 7 days',
+      noTasksLabel: 'No tasks for the next 7 days',
+      getTasks: () => {
+        function isNext7Days(date: Date) {
+          const today = startOfDay(new Date());
+          const tomorrow = addDays(today, 1);
+          const endOfNext7Days = addDays(tomorrow, 6);
+          return (
+            isWithinInterval(date, {
+              start: tomorrow,
+              end: endOfNext7Days,
+            }) && !isSameDay(date, tomorrow)
+          );
+        }
+        return tasks.filter(
+          (task) => task.dueDate !== null && isNext7Days(task.dueDate)
+        );
+      },
+    },
+    {
+      id: 'upcoming',
+      name: 'Upcoming',
+      noTasksLabel: 'No upcoming tasks',
+      getTasks: () => {
+        function isUpcoming(date: Date) {
+          const today = startOfDay(new Date());
+          const tomorrow = addDays(today, 1);
+          const endOfNext7Days = addDays(today, 7);
+          return (
+            isAfter(date, endOfNext7Days) ||
+            (isAfter(date, tomorrow) && !isNext7Days(date))
+          );
+        }
 
-      return db.tasks
-        .filter((task) => task.dueDate !== null && isToday(task.dueDate))
-        .primaryKeys();
-    },
-  },
-  {
-    id: "tomorrow",
-    name: "Tomorrow",
-    noTasksLabel: "No tasks for tomorrow",
-    loadTaskIds: async () => {
+        function isNext7Days(date: Date) {
+          const today = startOfDay(new Date());
+          const endOfNext7Days = addDays(today, 7);
+          return (
+            isWithinInterval(date, {
+              start: today,
+              end: endOfNext7Days,
+            }) && !isSameDay(date, today)
+          );
+        }
 
-  function isTomorrow(date: Date) {
-    const today = new Date();
-    const tomorrow = addDays(today, 1);
-    return isSameDay(date, tomorrow);
-  }
-      return db.tasks.filter((task) => task.dueDate !== null && isTomorrow(task.dueDate)).primaryKeys();
+        return tasks.filter(
+          (task) => task.dueDate !== null && isUpcoming(task.dueDate)
+        );
+      },
     },
-  },
-  {
-    id: "this-week",
-    name: "This week",
-    noTasksLabel: "No tasks for this week",
-    loadTaskIds: async () => {
-      function isRestOfWeek(date: Date) {
-        const today = new Date();
-        const startOfTomorrow = startOfDay(addDays(today, 1));
-        const startOfRestOfWeek = addDays(startOfTomorrow, 1);
-        const endOfRestOfWeek = endOfWeek(today);
-        return isWithinInterval(date, { start: startOfRestOfWeek, end: endOfRestOfWeek });
-      }
-      return db.tasks.filter((task) => task.dueDate !== null && isRestOfWeek(task.dueDate)).primaryKeys();
+    {
+      id: 'unscheduled',
+      name: 'Unscheduled',
+      noTasksLabel: 'No unscheduled tasks',
+      getTasks: () => {
+        return tasks.filter((task) => task.dueDate === null);
+      },
     },
-  },
-  {
-    id: "next-15-days",
-    name: "Next 15 days",
-    noTasksLabel: "No tasks for the next 15 days",
-    loadTaskIds: async () => {
-      function isUpcomingTwoWeeks(date: Date) {
-        const today = new Date();
-        const startOfNextWeek = startOfWeek(addWeeks(today, 1));
-        const endOfUpcomingTwoWeeks = endOfWeek(addWeeks(today, 2));
-        return isWithinInterval(date, { start: startOfNextWeek, end: endOfUpcomingTwoWeeks });
-      }
-      return db.tasks.filter((task) => task.dueDate !== null && isUpcomingTwoWeeks(task.dueDate)).primaryKeys();
-    },
-  },
-  {
-    id: "upcoming",
-    name: "Upcoming",
-    noTasksLabel: "No upcoming tasks",
-    loadTaskIds: async () => {
-      function isUpcoming(date: Date) {
-        const today = new Date();
-        const endOfUpcomingTwoWeeks = endOfWeek(addWeeks(today, 2));
-        return isAfter(date, endOfUpcomingTwoWeeks);
-      }
-      return db.tasks.filter((task) => task.dueDate !== null && isUpcoming(task.dueDate)).primaryKeys();
-    },
-  },
-  {
-    id: "unscheduled",
-    name: "Unscheduled",
-    noTasksLabel: "No unscheduled tasks",
-    loadTaskIds: async () => {
-      return db.tasks.filter(task => task.dueDate === null).primaryKeys();
-    },
-  },
-] satisfies Array<{
-  id: string;
-  name: string;
-  noTasksLabel: string;
-  loadTaskIds: () => Promise<Array<Task['id']>>;
-}>;
+  ] satisfies Array<{
+    id: string;
+    name: string;
+    noTasksLabel: string;
+    getTasks: () => Array<Task>;
+  }>;
+
+const tasksQueryKey = '/tasks';
+
+function mapArrayToEntities<
+  T extends Object[],
+  K extends string | number | symbol
+>(array: T, getId: (element: T[number]) => K) {
+  const ids = array.map(getId);
+  const record = array.reduce<Record<K, T[number]>>((record, element) => {
+    record[getId(element)] = element;
+    return record;
+  }, {} as Record<K, T[number]>);
+
+  return {
+    ids,
+    record,
+    toArray: () => ids.map((id) => record[id]),
+  };
+}
+
+function getTaskKey(task: Task) {
+  return task.id;
+}
 
 export function TasksPage() {
+  const tasksQuery = useSWR(tasksQueryKey, async () => {
+    const tasks = await db.tasks.toArray();
+    return mapArrayToEntities(tasks, getTaskKey);
+  });
+
+  if (tasksQuery.error) {
+    return <p>Error... {JSON.stringify(tasksQuery.error)}</p>;
+  }
+
+  if (!tasksQuery.data) {
+    return <p>Loading...</p>;
+  }
+
+  function handleTaskUpdated(updatedTask: Task) {
+    const updatedTasks = produce(tasksQuery.data!, (draft) => {
+      draft.record[updatedTask.id] = updatedTask;
+    });
+    tasksQuery.mutate(
+      async () => {
+        await db.tasks.update(updatedTask.id, updatedTask);
+        return updatedTasks;
+      },
+      {
+        optimisticData: updatedTasks,
+      }
+    );
+  }
+
+  function handleTaskDeleted(deletedTask: Task) {
+    const updatedTasks = produce(tasksQuery.data!, (draft) => {
+      delete draft.record[deletedTask.id];
+      draft.ids.splice(draft.ids.indexOf(deletedTask.id), 1);
+    });
+    tasksQuery.mutate(
+      async () => {
+        await db.tasks.delete(deletedTask.id);
+        return updatedTasks;
+      },
+      {
+        optimisticData: updatedTasks,
+      }
+    );
+  }
+
   return (
     <div className="flex min-h-screen w-full bg-background">
       <Sidebar />
@@ -119,21 +204,36 @@ export function TasksPage() {
         <main className="bg-gradient-to-br from-[#fdd6bd] to-[#f794a4] overflow-auto flex-1">
           <div className="p-4 md:p-6 max-w-screen-lg mx-auto">
             <div className="mb-4">
-              <CreateTaskForm onCreateTask={async (taskValues) => {
-                await db.tasks.add({
-                  completed: false,
-                  description: taskValues.description,
-                  dueDate: taskValues.dueDate,
-                  projectId: taskValues.projectId
-                })
-                await mutate(key => (key as string).startsWith('tasks'), undefined, {
-                  revalidate: true
-                })
-              }} />
+              <CreateTaskForm
+                onCreateTask={async (taskValues) => {
+                  const newTask = {
+                    completed: false,
+                    description: taskValues.description,
+                    dueDate: taskValues.dueDate,
+                    projectId: taskValues.projectId,
+                  };
+                  const newTaskId = await db.tasks.add(newTask);
+                  await tasksQuery.mutate(undefined, {
+                    revalidate: true,
+                  });
+                }}
+              />
             </div>
             <div className="grid gap-4">
-              {groups.map((group) => (
-                <TaskListGroup key={group.id} group={group} />
+              {getTaskGroups(tasksQuery.data?.toArray()).map((group) => (
+                <div>
+                  <h2 className="mb-2 text-lg font-semibold">{group.name}</h2>
+                  <div className="space-y-2">
+                    {group.getTasks().map((task) => (
+                      <TaskCard
+                        key={task.id}
+                        task={task}
+                        onTaskUpdated={handleTaskUpdated}
+                        onTaskDeleted={() => handleTaskDeleted(task)}
+                      />
+                    ))}
+                  </div>
+                </div>
               ))}
             </div>
           </div>
